2906742694a0807a4505fd3efe1bdbb89b9a3fc3
Roberto <robertosabau7@gmail.com>
2026-09-17 21:25:33 +0200

    Inicializacion Proyecto

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
-console.log("Hola Mundo");
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function crearVentana() {
+  // Configura las dimensiones de tu ventana nativa
+  const ventana = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: false
+    }
+  });
+  
+  ventana.setMenu(null); 
+
+  // Indica qué interfaz visual va a cargar dentro de la ventana
+  ventana.loadFile('index.html');
+}
+
+// Arranca la app cuando Electron esté listo
+app.whenReady().then(() => {
+  crearVentana();
+
+  // Comportamiento típico de Mac al reactivar la app
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) crearVentana();
+  });
+});
+
+// Cierra la app por completo si se cierran todas las ventanas (excepto en Mac)
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
